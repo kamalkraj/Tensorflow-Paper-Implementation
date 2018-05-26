81c5c55f7d1bd7f2d84d3c1640ff44daf5d776c6
Kamal Raj <kamalraj97@gmail.com>
2018-05-27 02:25:27 +0530

    updated code

--- cifar-10/model/model_keras.py
+++ cifar-10/model/model_keras.py
@@ -6,7 +6,7 @@ from keras.callbacks import LearningRateScheduler
 from keras.regularizers import l2
 
 
-def get_model():
+def get_model(weight_decay=0.0):
     model = Sequential()
     model.add(Conv2D(filters=20, kernel_size=[
               5, 5], activation='relu', padding='same', input_shape=(32, 32, 3)))
@@ -17,6 +17,10 @@ def get_model():
     model.add(Flatten())
     model.add(Dense(units=128, activation='relu'))
     model.add(Dense(units=10, activation='softmax'))
+    if weight_decay != 0.0 :
+        for layer in model.layers:
+            if hasattr(layer, 'kernel_regularizer'):
+                layer.kernel_regularizer = l2(weight_decay)
     return model
 
 
@@ -33,11 +37,8 @@ def main():
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     x_train = x_train/255.
     x_test = x_test/255.
-    model = get_model()
-    weight_decay = 5e-4
-    for layer in model.layers:
-        if hasattr(layer, 'kernel_regularizer'):
-            layer.kernel_regularizer = l2(weight_decay)
+    weight_decay = 0.0   #5e-4
+    model = get_model(weight_decay)
     sgd = SGD(lr=0.02, momentum=0.9)
     model.compile(
         optimizer=sgd, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
