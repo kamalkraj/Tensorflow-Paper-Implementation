80732bb4042c70c06830947bb1db3360313c6dc9
Kamal Raj <kamalraj97@gmail.com>
2018-05-27 01:05:15 +0530

    pep8

--- cifar-10/model/model_keras.py
+++ cifar-10/model/model_keras.py
@@ -1,43 +1,45 @@
 from keras.datasets import cifar10
 from keras.models import Sequential
-from keras.layers import Dense,Conv2D,MaxPool2D,Dropout,Flatten
+from keras.layers import Dense, Conv2D, MaxPool2D, Dropout, Flatten
 from keras.optimizers import SGD
 from keras.callbacks import LearningRateScheduler
 
 
 def get_model():
     model = Sequential()
-    model.add(Conv2D(filters=20,kernel_size=[5,5],activation='relu',padding='same',input_shape=(32,32,3)))
-    model.add(MaxPool2D(pool_size=(2,2),strides=2))
-    model.add(Conv2D(filters=50,kernel_size=[3,3],activation='relu',padding='same'))
-    model.add(MaxPool2D(pool_size=(2,2),strides=2))
+    model.add(Conv2D(filters=20, kernel_size=[
+              5, 5], activation='relu', padding='same', input_shape=(32, 32, 3)))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+    model.add(Conv2D(filters=50, kernel_size=[
+              3, 3], activation='relu', padding='same'))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=2))
     model.add(Flatten())
-    model.add(Dense(units=128,activation='relu'))
-    model.add(Dense(units=10,activation='softmax'))
+    model.add(Dense(units=128, activation='relu'))
+    model.add(Dense(units=10, activation='softmax'))
     return model
 
+
 def get_lrs(epoch):
-    if epoch< 40:
+    if epoch < 40:
         return float(0.02)
-    elif epoch >= 40 and epoch<=60:
+    elif epoch < 60:
         return float(0.005)
     else:
         return float(0.001)
 
+
 def main():
-    (x_train,y_train),(x_test,y_test) = cifar10.load_data()
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     x_train = x_train/255.
     x_test = x_test/255.
-    # import ipdb; ipdb.set_trace()
     model = get_model()
+    sgd = SGD(lr=0.02, momentum=0.9)
+    model.compile(
+        optimizer=sgd, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    lrs = LearningRateScheduler(get_lrs, verbose=1)
+    model.fit(x=x_train, y=y_train, batch_size=128, epochs=80, verbose=1,
+              shuffle=True, validation_data=(x_test, y_test), callbacks=[lrs])
 
-    sgd = SGD(lr=0.02,momentum=0.9)
-
-    model.compile(optimizer=sgd,loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-
-    lrs = LearningRateScheduler(get_lrs,verbose=1)
-
-    model.fit(x=x_train,y=y_train,batch_size=128,epochs=80,verbose=1,shuffle=True,validation_data=(x_test,y_test),callbacks=[lrs])
 
 if __name__ == '__main__':
     main()
