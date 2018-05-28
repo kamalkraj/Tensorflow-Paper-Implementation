9675ac2d722f4d23253e3a0137d75e9d088c77b2
Kamal Raj <kamalraj97@gmail.com>
2018-05-28 20:41:33 +0530

    added different lr rate

--- cifar-10/model/tf_model.py
+++ cifar-10/model/tf_model.py
@@ -3,10 +3,20 @@ import tensorflow as tf
 from sklearn.preprocessing import OneHotEncoder
 from tf_layers import conv2d,max_pooling2d,dense
 
+def get_lrs(epoch):
+    if epoch < 40:
+        return float(0.02)
+    elif epoch < 60:
+        return float(0.005)
+    else:
+        return float(0.001)
+
 def main(unused_argv):
 	def linear(x): return x
 	input_layer  = tf.placeholder(tf.float32,shape=[None,32,32,3],name='input_layer')
 	labels = tf.placeholder(tf.int64,shape=[None,],name='labels')
+
+	learning_rate = tf.placeholder(tf.float32,shape=[None],name='lr')
 
 	conv1 = conv2d(input_layer,3,20,[5,5],tf.nn.relu)
 	pool1 = max_pooling2d(conv1,[2,2],[2,2])
@@ -19,7 +29,7 @@ def main(unused_argv):
 	logits = dense(dense1,10,linear)
 
 	loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,labels=labels)
-	optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 	train_op = optimizer.minimize(loss=loss,global_step=tf.train.get_global_step())
 
 	correct_prediction = tf.equal(tf.argmax(tf.nn.softmax(logits),1),labels)
@@ -31,6 +41,7 @@ def main(unused_argv):
 	y_train = y_train.astype('int64').flatten()
 	y_test = y_test.astype('int64').flatten()
 	epochs = 80
+	lr = [0.02,0.005,0.001]
 	with tf.Session() as sess:
 		init = tf.global_variables_initializer()
 		sess.run(init)
@@ -40,10 +51,10 @@ def main(unused_argv):
 			batch_size = 128
 			for _ in range(391):
 				x = x_train[start:start+batch_size]
-				y = y_train[start:start+batch_size]
+				y = y_train[start:start+batch_size] 
 				start += batch_size
-				sess.run([train_op],feed_dict={input_layer:x,labels:y})
-			acc = sess.run(accuracy,feed_dict={input_layer:x_test,labels:y_test})
+				sess.run([train_op],feed_dict={input_layer:x,labels:y,learning_rate:get_lrs(epoch)})
+			acc = sess.run(accuracy,feed_dict={input_layer:x_test,labels:y_test,learning_rate:0.2})
 			print(acc)
 
 if __name__ == '__main__':
